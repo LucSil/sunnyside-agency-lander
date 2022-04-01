04646294fc737bcbbcf465aa18f712d7d629b3d0
LucSil <luciansilva351@gmail.com>
2022-04-01 11:36:20 +0100

    toggle logic implemented; styled navbar links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,26 @@
 <template>
   <header class="relative">
     <nav
-      class="px-6 absolute z-30 w-full mt-8 grid place-items-center transition-all"
+      class="px-6 absolute z-10 w-full mt-8 grid place-items-center transition-all"
     >
-      <div class="flex justify-between items-center w-full mb-9">
+      <div class="flex justify-between items-center w-full mb-10">
         <div>
           <img src="../assets/images/svg/logo.svg" />
         </div>
-        <button v-if="!clicked" @click="toggleMenu">
+        <button @click="toggleMenu">
           <img src="../assets/images/svg/icon-hamburger.svg" />
         </button>
-        <!-- <button v-if="clicked" @click="toggleMenu">
-          <img src="../assets/images/icon-close-menu.svg" />
-        </button> -->
       </div>
 
-      <ul class="bg-white w-full rounded-lg transition" v-if="menu">
+      <ul class="bg-N-White w-full grid place-items-center py-10" v-if="menu">
         <li>About</li>
-        <li class="border-y-[1px] border-neutral-300">Discover</li>
-        <li>Get Started</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <button
+          class="rounded-full bg-P-Yellow px-8 py-4 font-fraunces font-bold text-veryDarkDesatBlue"
+        >
+          CONTACT
+        </button>
       </ul>
     </nav>
 
@@ -38,3 +40,19 @@
     </picture>
   </header>
 </template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      menu: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      !this.menu ? (this.menu = true) : (this.menu = false);
+    },
+  },
+};
+</script>
