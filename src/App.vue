4d7f41b4915a3c09dd2841ec2986d6a676f12fd5
LucSil <luciansilva351@gmail.com>
2022-04-01 13:22:25 +0100

    imorted Egg in App

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <Header />
+  <Egg />
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import Egg from "@/components/Egg.vue";
 
 export default {
   name: "App",
-  components: { Header },
+  components: { Header, Egg },
 };
 </script>
