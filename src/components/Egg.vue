91c4992c8ec11f56ddf0360cb3b730041f376cf1
LucSil <luciansilva351@gmail.com>
2022-04-01 13:46:45 +0100

    templating completed for Egg

--- src/components/Egg.vue
+++ src/components/Egg.vue
@@ -1,1 +1,28 @@
-<template></template>
+<template>
+  <section>
+    <picture>
+      <source
+        media="(min-width:310px)"
+        srcset="../assets/images/backgrounds/egg-mobile.jpg"
+      />
+      <source
+        media="(min-width:750px)"
+        srcset="../assets/images/backgrounds/egg-mobile.jpg"
+      />
+      <img
+        src="../assets/images/backgrounds/egg-desktop.jpg"
+        style="width: auto"
+      />
+    </picture>
+
+    <div>
+      <h2>Transform your brand</h2>
+      <p>
+        We are a full-service creative agency specializing in helping brands
+        grow fast. Engage your clients through compelling visuals that do most
+        of the marketing for you.
+      </p>
+      <span><a>LEARN MORE</a></span>
+    </div>
+  </section>
+</template>
