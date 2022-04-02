5ca869da6eec2fcd9192d404abccbd94a445ab23
LucSil <luciansilva351@gmail.com>
2022-04-02 09:18:19 +0100

    completed styling for Cherry.vue

--- src/components/Cherry.vue
+++ src/components/Cherry.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="relative">
     <picture>
       <source
         media="(min-width:310px )"
@@ -11,9 +11,11 @@
       />
       <img src="../assets/images/backgrounds/cherry-desktop.jpg" />
     </picture>
-    <div>
-      <h2>Graphic Design</h2>
-      <p>
+    <div class="px-4 absolute text-base z-10 bottom-12 grid place-items-center">
+      <h2 class="mb-8 font-fraunces font-black text-3xl text-P-darkDesatCyan">
+        Graphic Design
+      </h2>
+      <p class="text-center font-barlow font-semibold text-P-darkDesatCyan">
         Great design makes you memorable. We deliver artwork that underscores
         your brand message and captures potential clients' attention.
       </p>
