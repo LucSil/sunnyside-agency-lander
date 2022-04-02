bd283ab382c0db86e655c5ed37534c126384257c
LucSil <luciansilva351@gmail.com>
2022-04-02 09:25:12 +0100

    imported Orange into App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <Egg />
   <Pink />
   <Cherry />
+  <Orange />
 </template>
 
 <script>
@@ -10,9 +11,10 @@ import Header from "@/components/Header.vue";
 import Egg from "@/components/Egg.vue";
 import Pink from "@/components/Pink.vue";
 import Cherry from "@/components/Cherry.vue";
+import Orange from "@/components/Orange.vue";
 
 export default {
   name: "App",
-  components: { Header, Egg, Pink, Cherry },
+  components: { Header, Egg, Pink, Cherry, Orange },
 };
 </script>
