ea28addfa9e9aa98d28e4126aaed5fd0848aa0de
LucSil <luciansilva351@gmail.com>
2022-04-02 09:50:25 +0100

    templating for Testimonial complete; imported Testimony into Testimonial.vue

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -1,6 +1,15 @@
-<template></template>
+<template>
+  <section>
+    <h2>CLIENT TESTIMONIALS</h2>
+    <Testimony />
+  </section>
+</template>
+
 <script>
+import Testimony from "./added/Testimony.vue";
+
 export default {
   name: "Testimonial",
+  components: { Testimony },
 };
 </script>
