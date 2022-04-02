66086423bb5b03ba620cde8ee55b99d2a54522d2
LucSil <luciansilva351@gmail.com>
2022-04-02 09:42:36 +0100

    imported Testimonial into App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <Pink />
   <Cherry />
   <Orange />
+  <Testimonial />
 </template>
 
 <script>
@@ -12,9 +13,10 @@ import Egg from "@/components/Egg.vue";
 import Pink from "@/components/Pink.vue";
 import Cherry from "@/components/Cherry.vue";
 import Orange from "@/components/Orange.vue";
+import Testimonial from "@/components/Testimonial.vue";
 
 export default {
   name: "App",
-  components: { Header, Egg, Pink, Cherry, Orange },
+  components: { Header, Egg, Pink, Cherry, Orange, Testimonial },
 };
 </script>
