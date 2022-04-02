a481d47fc269ab18f6f0d64cd6141f04174c21a0
LucSil <luciansilva351@gmail.com>
2022-04-02 08:56:02 +0100

    templating for Cherry completed

--- src/components/Cherry.vue
+++ src/components/Cherry.vue
@@ -1,4 +1,25 @@
-<template></template>
+<template>
+  <section>
+    <picture>
+      <source
+        media="(min-width:310px )"
+        srcset="../assets/images/backgrounds/cherry-mobile.jpg"
+      />
+      <source
+        media="(min-width:750 )"
+        srcset="../assets/images/backgrounds/cherry-desktop.jpg"
+      />
+      <img src="../assets/images/backgrounds/cherry-desktop.jpg" />
+    </picture>
+    <div>
+      <h2>Graphic Design</h2>
+      <p>
+        Great design makes you memorable. We deliver artwork that underscores
+        your brand message and captures potential clients' attention.
+      </p>
+    </div>
+  </section>
+</template>
 <script>
 export default {
   name: "Cherry",
