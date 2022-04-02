e256d7abacab80d18b6f95c0e7b56b91313f42a2
LucSil <luciansilva351@gmail.com>
2022-04-02 08:47:36 +0100

    imported Cherry into App

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   <Header />
   <Egg />
   <Pink />
+  <Cherry />
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
 import Egg from "@/components/Egg.vue";
 import Pink from "@/components/Pink.vue";
+import Cherry from "@/components/Cherry.vue";
 
 export default {
   name: "App",
-  components: { Header, Egg, Pink },
+  components: { Header, Egg, Pink, Cherry },
 };
 </script>
