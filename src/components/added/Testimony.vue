2fc48187d6b57f966809509a147d5a27a0589366
LucSil <luciansilva351@gmail.com>
2022-04-02 21:35:01 +0100

    initial templating for Testimony.vue

--- src/components/added/Testimony.vue
+++ src/components/added/Testimony.vue
@@ -1,4 +1,20 @@
-<template></template>
+<template>
+  <section>
+    <div>
+      <div>
+        <img src="../../assets/images/image-emily.jpg" />
+      </div>
+      <p>
+        We put our trust in Sunnyside and they delivered, making sure our needs
+        were met and deadlines were always hit.
+      </p>
+      <div>
+        <span>Emily R.</span>
+        <span>Marketing Director</span>
+      </div>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {
