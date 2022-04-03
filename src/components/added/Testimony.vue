28ac28c8e1b39386fef683787bf80ebfd9ad9233
LucSil <luciansilva351@gmail.com>
2022-04-03 14:35:33 +0100

    completed templating and styling for Testimony.vue

--- src/components/added/Testimony.vue
+++ src/components/added/Testimony.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <div class="testi">
+    <div class="testi mb-16">
       <div class="imgDiv">
         <img src="../../assets/images/image-emily.jpg" />
       </div>
@@ -11,6 +11,32 @@
       <div class="id">
         <span class="name">Emily R.</span>
         <span class="post">Marketing Director</span>
+      </div>
+    </div>
+    <div class="testi mb-16">
+      <div class="imgDiv">
+        <img src="../../assets/images/image-thomas.jpg" />
+      </div>
+      <p>
+        Sunnyside's enthusiasm coupled with their keen interest in our brand's
+        success made it a satisfying and enjoyable experience.
+      </p>
+      <div class="id">
+        <span class="name">Thomas S.</span>
+        <span class="post">Chief Operating Officer</span>
+      </div>
+    </div>
+    <div class="testi">
+      <div class="imgDiv">
+        <img src="../../assets/images/image-jennie.jpg" />
+      </div>
+      <p>
+        Incredible end result! Our sales increased over 400% when we worked with
+        Sunnyside. Highly recommended!
+      </p>
+      <div class="id">
+        <span class="name">Jennie F.</span>
+        <span class="post">Business Owner</span>
       </div>
     </div>
   </section>
