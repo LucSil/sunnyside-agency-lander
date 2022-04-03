8d403f45de0d45fe11e1ca20d12f9ec219fef4ab
LucSil <luciansilva351@gmail.com>
2022-04-03 14:52:11 +0100

    templating for Images complete

--- src/components/Images.vue
+++ src/components/Images.vue
@@ -1,4 +1,55 @@
-<template></template>
+<template>
+  <section>
+    <div>
+      <picture>
+        <source
+          media="(min-width:310px )"
+          srcset="../assets/images/backgrounds/milkbottles-mobile.jpg"
+        />
+        <source
+          media="(min-width:750px )"
+          srcset="../assets/images/backgrounds/milkbottles-desktop.jpg"
+        />
+        <img src="../assets/images/backgrounds/milkbottles-desktop.jpg" />
+      </picture>
+      <picture>
+        <source
+          media="(min-width:310px )"
+          srcset="../assets/images/backgrounds/orange-mobile.jpg"
+        />
+        <source
+          media="(min-width:750px )"
+          srcset="../assets/images/backgrounds/orange-desktop.jpg"
+        />
+        <img src="../assets/images/backgrounds/orange-desktop.jpg" />
+      </picture>
+    </div>
+    <div>
+      <picture>
+        <source
+          media="(min-width:310px )"
+          srcset="../assets/images/backgrounds/cone-mobile.jpg"
+        />
+        <source
+          media="(min-width:750px )"
+          srcset="../assets/images/backgrounds/cone-desktop.jpg"
+        />
+        <img src="../assets/images/backgrounds/cone-desktop.jpg" />
+      </picture>
+      <picture>
+        <source
+          media="(min-width:310px )"
+          srcset="../assets/images/backgrounds/sugarcubes-mobile.jpg"
+        />
+        <source
+          media="(min-width:750px )"
+          srcset="../assets/images/backgrounds/sugarcubes-desktop.jpg"
+        />
+        <img src="../assets/images/backgrounds/sugarcubes-desktop.jpg" />
+      </picture>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {
