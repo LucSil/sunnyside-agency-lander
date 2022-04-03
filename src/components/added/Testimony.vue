3daa8576b6011c2e84f98c9b7e708d93c79eac6e
LucSil <luciansilva351@gmail.com>
2022-04-03 14:24:02 +0100

    moved classes from Testimony into app.css under common class names for duplication

--- src/components/added/Testimony.vue
+++ src/components/added/Testimony.vue
@@ -1,22 +1,16 @@
 <template>
   <section>
-    <div class="grid place-items-center">
-      <div class="w-16 h-16 mb-8">
-        <img class="rounded-full" src="../../assets/images/image-emily.jpg" />
+    <div class="testi">
+      <div class="imgDiv">
+        <img src="../../assets/images/image-emily.jpg" />
       </div>
-      <p
-        class="text-center font-barlow mx-6 mb-8 text-veryDarkGrayishBlue font-semibold"
-      >
+      <p>
         We put our trust in Sunnyside and they delivered, making sure our needs
         were met and deadlines were always hit.
       </p>
-      <div class="grid place-items-center">
-        <span class="mb-1 font-fraunces font-bold text-veryDarkDesatBlue"
-          >Emily R.</span
-        >
-        <span class="font-barlow opacity-75 text-sm text-darkGrayishBlue"
-          >Marketing Director</span
-        >
+      <div class="id">
+        <span class="name">Emily R.</span>
+        <span class="post">Marketing Director</span>
       </div>
     </div>
   </section>
