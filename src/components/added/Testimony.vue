75c862fecb06d6ae7f67df1e2587f6ef7bb47cd5
LucSil <luciansilva351@gmail.com>
2022-04-03 14:01:33 +0100

    initial styling for Testimony

--- src/components/added/Testimony.vue
+++ src/components/added/Testimony.vue
@@ -1,16 +1,22 @@
 <template>
   <section>
-    <div>
-      <div>
-        <img src="../../assets/images/image-emily.jpg" />
+    <div class="grid place-items-center">
+      <div class="w-16 h-16 mb-8">
+        <img class="rounded-full" src="../../assets/images/image-emily.jpg" />
       </div>
-      <p>
+      <p
+        class="text-center font-barlow mx-6 mb-8 text-veryDarkGrayishBlue font-semibold"
+      >
         We put our trust in Sunnyside and they delivered, making sure our needs
         were met and deadlines were always hit.
       </p>
-      <div>
-        <span>Emily R.</span>
-        <span>Marketing Director</span>
+      <div class="grid place-items-center">
+        <span class="mb-2 font-fraunces font-bold text-veryDarkDesatBlue"
+          >Emily R.</span
+        >
+        <span class="font-barlow opacity-75 text-sm text-darkGrayishBlue"
+          >Marketing Director</span
+        >
       </div>
     </div>
   </section>
