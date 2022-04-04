8cdae98928f729cf0ca38d3e9ba037aa88438f37
LucSil <luciansilva351@gmail.com>
2022-04-04 07:41:56 +0100

    templating for footer complete

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,33 @@
-<template></template>
+<template>
+  <section>
+    <h3>sunnyside</h3>
+    <ul>
+      <li>
+        <a href="">About</a>
+      </li>
+      <li>
+        <a href="">Services</a>
+      </li>
+      <li>
+        <a href="">Projects</a>
+      </li>
+    </ul>
+    <div>
+      <a href="">
+        <img src="../assets/images/svg/icon-facebook.svg" />
+      </a>
+      <a href="">
+        <img src="../assets/images/svg/icon-instagram.svg" />
+      </a>
+      <a href="">
+        <img src="../assets/images/svg/icon-twitter.svg" />
+      </a>
+      <a href="">
+        <img src="../assets/images/svg/icon-pinterest.svg" />
+      </a>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {
